eac9da1bf961ca5b22dd615cd4e76a5b9b11a0ca
Simon Prochazka <simon@fallion.net>
2020-04-06 23:47:44 +0200

    Revert "feat: add sample healthcheck route"
    This reverts commit eb76cda84ddcec732cd41908e0cd74561d74279b.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,25 +2,8 @@ package main
 
 import (
 	"log"
-	"net"
-	"net/http"
-	"time"
-
-	"github.com/commitsar-app/merge-master/api/rest/v1"
 )
 
 func main() {
-	restServer := rest.NewServer("merge-master")
-
-	address := "localhost"
-	port := "3000"
-	serveAddr := net.JoinHostPort(address, port)
-	server := &http.Server{
-		Handler:      restServer.Router,
-		Addr:         serveAddr,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-
-	log.Fatal(server.ListenAndServe())
+	log.Println("hello world")
 }
