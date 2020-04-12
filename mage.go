e2764177e78b52a9eb0ed0e682fdb52a926cb445
Simon Prochazka <simon@fallion.net>
2020-04-12 14:02:36 +0200

    chore: move oto template into /templates directory
    - adds build:server command

--- mage.go
+++ mage.go
@@ -3,6 +3,8 @@
 package main
 
 import (
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
 	"github.com/outillage/magefiles"
 )
 
@@ -20,4 +22,22 @@ func Test() error {
 
 func GoModTidy() error {
 	return magefiles.GoModTidy()
+}
+
+type Build mg.Namespace
+
+func (Build) Server() error{
+	var args []string
+
+	args = append(args, "-template", "./templates/server.go.plush")
+
+	args = append(args, "-out", "./api/generated/oto.gen.go")
+
+	args = append(args, "-ignore", "Ignorer")
+
+	args = append(args, "-pkg", "generated")
+
+	args = append(args, "./api/definitions")
+
+	return sh.RunV("oto", args...)
 }
